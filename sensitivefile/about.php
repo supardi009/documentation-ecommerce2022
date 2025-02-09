ff119a978b8ce6812353f3504666d4e33d7c2958
bebekdeathcore <56356388+supardi009@users.noreply.github.com>
2025-02-09 15:25:05 +0700

    Add files via upload

---
+++ sensitivefile/about.php
@@ -0,0 +1,17 @@
+<?php 
+	include 'header.php';
+ ?>
+
+
+<div class="container" style="padding-bottom: 300px;">
+	<h2 style=" width: 100%; border-bottom: 4px solid #DAA520"><b>Tentang Kami</b></h2>
+	<p class="text-justify">Toko Roti adalah sebuah contoh toko roti online untuk berbisnis dengan menjual macam - macam roti. Dibuat 2022, dan produk kami sehat, bergizi, dan terjangkau.</p>
+<p> Toko Roti ini dikelola oleh admin dan sebagai pelanggan berikut tampilan pemesananan.</p>
+</div>
+
+
+
+
+ <?php 
+	include 'footer.php';
+ ?>
---
+++ sensitivefile/footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #DAA520;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #FF7F50"><b>Toko Roti</b></h3>
+					<p>Jalan Raya Legok</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +628128059384</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> contoh@mail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b>Menu</b></h5>
+					<p><a href=""  style="color: #000">Produk</a></p>
+					<p><a href=""  style="color: #000">Tentang kami</a></p>
+					<p><a href=""  style="color: #000">Hubungi Kami</a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #DAA520; padding: 5px; color: #fff; text-align: center;">
+			<span>Uji Komputer</span>
+		</div>
+	</footer>
+
+</body>
+</html>
